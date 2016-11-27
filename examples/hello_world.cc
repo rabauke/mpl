0f29bcc2883ac43de53875a0c04498195f64b5f5
Heiko Bauke <heiko.bauke@mail.de>
2016-11-27 19:07:12 +0100

    comments for hello_world

--- examples/hello_world.cc
+++ examples/hello_world.cc
@@ -1,10 +1,14 @@
 #include <cstdlib>
 #include <iostream>
+// include MPL header file
 #include <mpl/mpl.hpp>
 
 int main() {
+  // get a reference to communicator "world"
   const mpl::communicator & comm_world(mpl::environment::comm_world());
-
+  // each process prints a message containg processor name, rank in
+  // communicator world and size of communicator world
+  // output may depend on MPI implementation
   std::cout << "Hello world! I am running on \""
    	    << mpl::environment::processor_name()
    	    << "\". My rank is "
