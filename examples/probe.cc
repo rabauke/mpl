f7d1fc98473fa0f975e144c57cf22c8061eda05b
Heiko Bauke <heiko.bauke@mail.de>
2016-11-28 18:20:16 +0100

    comments for probe

--- examples/probe.cc
+++ examples/probe.cc
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
-#include <algorithm>
 #include <mpl/mpl.hpp>
 
 template<typename I>
@@ -17,10 +16,12 @@ void print_range(const char * const str, I i1, I i2) {
 
 int main() {
   const mpl::communicator &comm_world=mpl::environment::comm_world();
-  if (comm_world.size()<2) 
+  // run the program with two ore more processes
+  if (comm_world.size()<2)
     return EXIT_FAILURE;
-  const int tag=29;
   if (comm_world.rank()==0) {
+    // send a message of n elements to rank 1
+    const int tag=29;
     const int n=12;
     std::vector<int> v(n);
     mpl::contiguous_layout<int> l(n);
@@ -28,14 +29,19 @@ int main() {
     comm_world.send(v.data(), l, 1, tag);
   }
   if (comm_world.rank()==1) {
+    // receive a message of an a priory unknown number of elements from rank 0
+    // first probe for a message from some arbitrary rank with any tag
     mpl::status s(comm_world.probe(mpl::environment::any_source(), mpl::environment::any_tag()));
+    // decode the number of elements, the source and the tag
     int n(s.get_count<int>()), source(s.source()), tag(s.tag());
     std::cerr << "souce : " << s.source() << '\n'
 	      << "tag   : " << s.tag() << '\n'
 	      << "error : " << s.error() << '\n'
 	      << "count : " << n << '\n';
+    // reserve sufficinet amount of memory to receive the message
     std::vector<int> v(n);
     mpl::contiguous_layout<int> l(n);
+    // finally receive the message
     comm_world.recv(v.data(), l, source, tag);
     print_range("v = ", v.begin(), v.end());
   }
