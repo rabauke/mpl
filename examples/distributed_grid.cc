a29770c839d3260d6081cdb8a12a3c14f359a235
Heiko Bauke <heiko.bauke@mail.de>
2016-11-29 23:30:28 +0100

    comments for distributed_grid.cc

--- examples/distributed_grid.cc
+++ examples/distributed_grid.cc
@@ -21,37 +21,56 @@ void update_overlap(const mpl::cart_communicator &C, mpl::distributed_grid<dim, 
 int main() {
   const mpl::communicator & comm_world(mpl::environment::comm_world());
   {
+    // build a one-dimensional Cartesian communicator
+    // Cartesian is non-cyclic
     mpl::cart_communicator::sizes sizes( {{0, false}} );
-    mpl::cart_communicator comm_c(comm_world, 
+    mpl::cart_communicator comm_c(comm_world,
 				  mpl::dims_create(comm_world.size(), sizes));
+    // create a distributed grid of 31 total grid points and 2 shadow grid points
+    // to mirror data between adjacent processes
     mpl::distributed_grid<1, int> G(comm_c, { {31, 2} });
+    // fill local grid including shadow grid points
     for (auto i=G.obegin(0), i_end=G.oend(0); i<i_end; ++i)
       G(i)=comm_c.rank();
+    // get shadow data from adjacent processes
     update_overlap(comm_c, G);
-    for (auto i=G.obegin(0), i_end=G.oend(0); i<i_end; ++i)
-      std::cout << G(i);
-    std::cout << std::endl;
+    // print local grid including shadow grid points
+    for (int k=0; k<comm_c.size(); ++k) {
+      if (k==comm_c.rank()) {
+	for (auto i=G.obegin(0), i_end=G.oend(0); i<i_end; ++i)
+	  std::cout << G(i);
+	std::cout << std::endl;
+      }
+      comm_c.barrier();  // barrier may avoid overlapping output
+    }
   }
   {
+    // build a two-dimensional Cartesian communicator
+    // Cartesian is cyclic along 1st dimension, non-cyclic along 2nd dimension
     mpl::cart_communicator::sizes sizes( {{0, true}, {0, false}} );
-    mpl::cart_communicator comm_c(comm_world, 
-				  mpl::dims_create(comm_world.size(), sizes));
-    mpl::distributed_grid<2, int> G(comm_c, 
-				    {{11, 2}, {13, 1}});
+    mpl::cart_communicator comm_c(comm_world,
+  				  mpl::dims_create(comm_world.size(), sizes));
+    // create a distributed grid of 11x13 total grid points and 2 respectively 1
+    // shadow grid points to mirror data between adjacent processes
+    mpl::distributed_grid<2, int> G(comm_c,
+  				    {{11, 2}, {13, 1}});
+    // fill local grid including shadow grid points
     for (auto j=G.obegin(1), j_end=G.oend(1); j<j_end; ++j)
       for (auto i=G.obegin(0), i_end=G.oend(0); i<i_end; ++i)
-	G(i, j)=comm_c.rank();
+  	G(i, j)=comm_c.rank();
+    // get shadow data from adjacent processes
     update_overlap(comm_c, G);
-    for (int i=0; i<comm_c.size(); ++i) {
-      if (i==comm_c.rank()) {
-	std::cout << std::endl;
-	for (auto j=G.obegin(1), j_end=G.oend(1); j<j_end; ++j) {
-	  for (auto i=G.obegin(0), i_end=G.oend(0); i<i_end; ++i) 
-	    std::cout << G(i, j);
-	  std::cout << std::endl;
-	}
+    // print local grid including shadow grid points
+    for (int k=0; k<comm_c.size(); ++k) {
+      if (k==comm_c.rank()) {
+  	std::cout << std::endl;
+  	for (auto j=G.obegin(1), j_end=G.oend(1); j<j_end; ++j) {
+  	  for (auto i=G.obegin(0), i_end=G.oend(0); i<i_end; ++i)
+  	    std::cout << G(i, j);
+  	  std::cout << std::endl;
+  	}
       }
-      comm_c.barrier();
+      comm_c.barrier();  // barrier may avoid overlapping output
     }
   }
   return EXIT_SUCCESS;
