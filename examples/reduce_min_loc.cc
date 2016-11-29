440ac513568485ccb7896966b08a31100453f6b1
Heiko Bauke <heiko.bauke@mail.de>
2016-11-29 18:55:19 +0100

    comments for reduce_min_loc

--- examples/reduce_min_loc.cc
+++ examples/reduce_min_loc.cc
@@ -3,8 +3,10 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <algorithm>
 #include <mpl/mpl.hpp>
 
+// data type to store data an position of global minimum
 typedef std::pair<double, int> pair_t;
 
 int main() {
@@ -12,34 +14,37 @@ int main() {
   // generate data
   std::srand(std::time(0)*comm_world.rank());  // random seed
   const int n=8;
+  // populate vector with random data
   std::vector<pair_t> v(n);
-  for (pair_t &i : v)
-    i=std::make_pair(static_cast<double>(std::rand())/RAND_MAX, comm_world.rank());
-  // calculate minium and its location and send result to rank 0
+  std::generate(v.begin(), v.end(), [&]()->auto{
+      return std::make_pair(static_cast<double>(std::rand())/RAND_MAX, comm_world.rank());
+    });
+  // calculate minium and its location and send result to rank root
+  int root=0;
   mpl::contiguous_layout<pair_t> layout(n);
-  if (comm_world.rank()==0) {
+  if (comm_world.rank()==root) {
     std::vector<pair_t> result(n);
     // calculate minimum
-    comm_world.reduce(mpl::min<pair_t>(), 0, v.data(), result.data(), layout);
+    comm_world.reduce(mpl::min<pair_t>(), root, v.data(), result.data(), layout);
     // display data from all ranks
-    std::cout << "Arguments:\n";
+    std::cout << "arguments:\n";
     for (int r=0; r<comm_world.size(); ++r) {
       if (r>0)
 	comm_world.recv(v.data(), layout, r);
-      for (pair_t i : v) 
+      for (auto i : v)
 	std::cout << std::fixed << std::setprecision(5) << i.first << ' ' << i.second << '\t';
       std::cout << '\n';
     }
     // display results of global reduction
-    std::cout << "\nResults:\n";
-    for (pair_t i : result) 
+    std::cout << "\nresults:\n";
+    for (pair_t i : result)
       std::cout << std::fixed << std::setprecision(5) << i.first << ' ' << i.second << '\t';
     std::cout << '\n';
   } else {
     // calculate minium and its location and send result to rank 0
-    comm_world.reduce(mpl::min<pair_t>(), 0, v.data(), layout);
+    comm_world.reduce(mpl::min<pair_t>(), root, v.data(), layout);
     // send data to rank 0 for display
-    comm_world.send(v.data(), layout, 0);
+    comm_world.send(v.data(), layout, root);
   }
   return EXIT_SUCCESS;
 }
