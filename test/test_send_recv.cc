ebe3bbd2804210d94e24596b5d048980463a2491
Heiko Bauke <heiko.bauke@mail.de>
2018-03-02 12:39:08 +0100

    add test for sendrecv_replace

--- test/test_send_recv.cc
+++ test/test_send_recv.cc
@@ -81,6 +81,17 @@ bool sendrecv_test() {
   return x[0]==T((rank-1+size)%size) and x[1]==T(rank);
 }
 
+template<typename T>
+bool sendrecv_replace_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int rank=comm_world.rank();
+  int size=comm_world.size();
+  T x{T(rank)};
+  comm_world.sendrecv_replace(x, (rank+1)%size, mpl::tag(0),
+                              (rank-1+size)%size, mpl::tag(0));
+  return x==T((rank-1+size)%size);
+}
+
 
 BOOST_AUTO_TEST_CASE(send_recv) {
   // integer types
@@ -255,4 +266,35 @@ BOOST_AUTO_TEST_CASE(sendrecv) {
   BOOST_TEST(sendrecv_test<long double>());
   // logical type
   BOOST_TEST(sendrecv_test<bool>());
+}
+
+BOOST_AUTO_TEST_CASE(sendrecv_replace) {
+  // integer types
+#if __cplusplus>=201703L
+  BOOST_TEST(sendrecv_replace_test<std::byte>());
+#endif
+  BOOST_TEST(sendrecv_replace_test<char>());
+  BOOST_TEST(sendrecv_replace_test<signed char>());
+  BOOST_TEST(sendrecv_replace_test<unsigned char>());
+  BOOST_TEST(sendrecv_replace_test<signed short>());
+  BOOST_TEST(sendrecv_replace_test<unsigned short>());
+  BOOST_TEST(sendrecv_replace_test<signed int>());
+  BOOST_TEST(sendrecv_replace_test<unsigned int>());
+  BOOST_TEST(sendrecv_replace_test<signed long>());
+  BOOST_TEST(sendrecv_replace_test<unsigned long>());
+  BOOST_TEST(sendrecv_replace_test<signed long long>());
+  BOOST_TEST(sendrecv_replace_test<unsigned long long>());
+  // character types
+  BOOST_TEST(sendrecv_replace_test<wchar_t>());
+  BOOST_TEST(sendrecv_replace_test<char16_t>());
+  BOOST_TEST(sendrecv_replace_test<char32_t>());
+  // floating point number types
+  BOOST_TEST(sendrecv_replace_test<float>());
+  BOOST_TEST(sendrecv_replace_test<double>());
+  BOOST_TEST(sendrecv_replace_test<long double>());
+  BOOST_TEST(sendrecv_replace_test<float>());
+  BOOST_TEST(sendrecv_replace_test<double>());
+  BOOST_TEST(sendrecv_replace_test<long double>());
+  // logical type
+  BOOST_TEST(sendrecv_replace_test<bool>());
 }
