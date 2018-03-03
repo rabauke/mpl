3d72cb691fb09f7325850ec8459c881a9ce64a6c
Heiko Bauke <heiko.bauke@mail.de>
2018-03-03 20:23:59 +0100

    add test_exscan.cc

---
+++ test/test_exscan.cc
@@ -0,0 +1,78 @@
+#define BOOST_TEST_MODULE exscan
+
+#include <boost/test/included/unit_test.hpp>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <utility>
+#include <algorithm>
+#include <mpl/mpl.hpp>
+
+template<typename T>
+T add(const T &a, const T &b) {
+  return a+b;
+}
+
+template<typename T>
+bool exscan_func_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) }, y;
+  comm_world.exscan(add<T>, x, y);
+  return comm_world.rank()==0 ? true : y==T((N*N-N)/2);
+}
+
+template<typename T>
+bool exscan_op_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) }, y;
+  comm_world.exscan(mpl::plus<T>(), x, y);
+  return comm_world.rank()==0 ? true : y==T((N*N-N)/2);
+}
+
+template<typename T>
+bool exscan_lambda_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) }, y;
+  comm_world.exscan([](T a, T b) { return a+b; }, x, y);
+  return comm_world.rank()==0 ? true : y==T((N*N-N)/2);
+}
+
+template<typename T>
+bool exscan_inplace_func_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) };
+  comm_world.exscan(add<T>, x);
+  return comm_world.rank()==0 ? true : x==T((N*N-N)/2);
+}
+
+template<typename T>
+bool exscan_inplace_op_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) };
+  comm_world.exscan(mpl::plus<T>(), x);
+  return comm_world.rank()==0 ? true : x==T((N*N-N)/2);
+}
+
+template<typename T>
+bool exscan_inplace_lambda_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) };
+  comm_world.exscan([](T a, T b) { return a+b; }, x);
+  return comm_world.rank()==0 ? true : x==T((N*N-N)/2);
+}
+
+
+BOOST_AUTO_TEST_CASE(reduce) {
+  BOOST_TEST(exscan_func_test<double>());
+  BOOST_TEST(exscan_op_test<double>());
+  BOOST_TEST(exscan_lambda_test<double>());
+  BOOST_TEST(exscan_inplace_func_test<double>());
+  BOOST_TEST(exscan_inplace_op_test<double>());
+  BOOST_TEST(exscan_inplace_lambda_test<double>());
+}
