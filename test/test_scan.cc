f1a7f884e4c530c8b8fb561d47750559a67eea1c
Heiko Bauke <heiko.bauke@mail.de>
2018-03-03 20:16:02 +0100

    add test_scan.cc

---
+++ test/test_scan.cc
@@ -0,0 +1,78 @@
+#define BOOST_TEST_MODULE scan
+
+#include <boost/test/included/unit_test.hpp>
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <utility>
+#include <algorithm>
+#include <mpl/mpl.hpp>
+
+template<typename T>
+T add(const T &a, const T &b) {
+  return a+b;
+}
+
+template<typename T>
+bool scan_func_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) }, y;
+  comm_world.scan(add<T>, x, y);
+  return y==T((N*N+N)/2);
+}
+
+template<typename T>
+bool scan_op_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) }, y;
+  comm_world.scan(mpl::plus<T>(), x, y);
+  return y==T((N*N+N)/2);
+}
+
+template<typename T>
+bool scan_lambda_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) }, y;
+  comm_world.scan([](T a, T b) { return a+b; }, x, y);
+  return y==T((N*N+N)/2);
+}
+
+template<typename T>
+bool scan_inplace_func_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) };
+  comm_world.scan(add<T>, x);
+  return x==T((N*N+N)/2);
+}
+
+template<typename T>
+bool scan_inplace_op_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) };
+  comm_world.scan(mpl::plus<T>(), x);
+  return x==T((N*N+N)/2);
+}
+
+template<typename T>
+bool scan_inplace_lambda_test() {
+  const mpl::communicator &comm_world=mpl::environment::comm_world();
+  int N=comm_world.rank()+1;
+  T x{ T(N) };
+  comm_world.scan([](T a, T b) { return a+b; }, x);
+  return x==T((N*N+N)/2);
+}
+
+
+BOOST_AUTO_TEST_CASE(reduce) {
+  BOOST_TEST(scan_func_test<double>());
+  BOOST_TEST(scan_op_test<double>());
+  BOOST_TEST(scan_lambda_test<double>());
+  BOOST_TEST(scan_inplace_func_test<double>());
+  BOOST_TEST(scan_inplace_op_test<double>());
+  BOOST_TEST(scan_inplace_lambda_test<double>());
+}
