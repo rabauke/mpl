7b1db369ba922bbcc567796c9dc06a6ba87a8bed
Heiko Bauke <heiko.bauke@mail.de>
2021-08-19 01:49:09 +0200

    unit test refactoring for clarity

--- test/test_communicator_gatherv.cc
+++ test/test_communicator_gatherv.cc
@@ -9,18 +9,26 @@ template<typename T>
 bool gatherv_test(const T &val) {
   const mpl::communicator &comm_world{mpl::environment::comm_world()};
   const int N{(comm_world.size() * comm_world.size() + comm_world.size()) / 2};
-  std::vector<T> v1(N), v2(N);
-  std::iota(begin(v1), end(v1), val);
-  mpl::layouts<T> l;
+  std::vector<T> v_gather(N);
+  std::vector<T> v_send(comm_world.rank() + 1);
+  std::vector<T> v_expected(N);
+  std::iota(begin(v_expected), end(v_expected), val);
+  mpl::layouts<T> layouts;
   for (int i{0}, i_end{comm_world.size()}, offset{0}; i < i_end; ++i) {
-    l.push_back(mpl::indexed_layout<T>({{i + 1, offset}}));
+    layouts.push_back(mpl::indexed_layout<T>({{i + 1, offset}}));
     offset += i + 1;
   }
+  T t_val{val};
+  for (int i{0}, i_end{(comm_world.rank() * comm_world.rank() + comm_world.rank()) / 2};
+       i < i_end; ++i)
+    ++t_val;
+  std::iota(begin(v_send), end(v_send), t_val);
+  mpl::vector_layout<T> layout(comm_world.rank() + 1);
   if (comm_world.rank() == 0) {
-    comm_world.gatherv(0, v1.data(), l[0], v2.data(), l);
-    return v1 == v2;
+    comm_world.gatherv(0, v_send.data(), layout, v_gather.data(), layouts);
+    return v_gather == v_expected;
   } else {
-    comm_world.gatherv(0, v1.data(), l[comm_world.rank()]);
+    comm_world.gatherv(0, v_send.data(), layout);
     return true;
   }
 }
@@ -30,19 +38,27 @@ template<typename T>
 bool igatherv_test(const T &val) {
   const mpl::communicator &comm_world{mpl::environment::comm_world()};
   const int N{(comm_world.size() * comm_world.size() + comm_world.size()) / 2};
-  std::vector<T> v1(N), v2(N);
-  std::iota(begin(v1), end(v1), val);
-  mpl::layouts<T> l;
+  std::vector<T> v_gather(N);
+  std::vector<T> v_send(comm_world.rank() + 1);
+  std::vector<T> v_expected(N);
+  std::iota(begin(v_expected), end(v_expected), val);
+  mpl::layouts<T> layouts;
   for (int i{0}, i_end{comm_world.size()}, offset{0}; i < i_end; ++i) {
-    l.push_back(mpl::indexed_layout<T>({{i + 1, offset}}));
+    layouts.push_back(mpl::indexed_layout<T>({{i + 1, offset}}));
     offset += i + 1;
   }
+  T t_val{val};
+  for (int i{0}, i_end{(comm_world.rank() * comm_world.rank() + comm_world.rank()) / 2};
+       i < i_end; ++i)
+    ++t_val;
+  std::iota(begin(v_send), end(v_send), t_val);
+  mpl::vector_layout<T> layout(comm_world.rank() + 1);
   if (comm_world.rank() == 0) {
-    auto r{comm_world.igatherv(0, v1.data(), l[0], v2.data(), l)};
+    auto r{comm_world.igatherv(0, v_send.data(), layout, v_gather.data(), layouts)};
     r.wait();
-    return v1 == v2;
+    return v_gather == v_expected;
   } else {
-    auto r{comm_world.igatherv(0, v1.data(), l[comm_world.rank()])};
+    auto r{comm_world.igatherv(0, v_send.data(), layout)};
     r.wait();
     return true;
   }
@@ -59,9 +75,8 @@ struct tuple {
   }
 };
 
-bool operator==(const tuple&t1, const tuple &t2)
-{
-  return t1.a== t2.a and t1.b == t2.b;
+bool operator==(const tuple &t1, const tuple &t2) {
+  return t1.a == t2.a and t1.b == t2.b;
 }
 
 MPL_REFLECTION(tuple, a, b)
