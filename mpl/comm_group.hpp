39b28017cf85418c1c65f273026f1ba57b3d7938
Heiko Bauke <heiko.bauke@mail.de>
2021-09-13 22:22:39 +0200

    tests and documentation for allreduce

--- mpl/comm_group.hpp
+++ mpl/comm_group.hpp
@@ -3987,12 +3987,33 @@ namespace mpl {
 
     // === all-reduce ===
     // --- blocking all-reduce ---
+    /// \brief Performs a reduction operation over all processes and scatters the result.
+    /// \tparam F type representing the reduction operation, reduction operation is performed
+    /// on data of type T
+    /// \tparam T type of input and output data of the reduction operation, must meet the
+    /// requirements as described in the \ref data_types "data types" section
+    /// \param f reduction operation
+    /// \param senddata input data for the reduction operation
+    /// \param recvdata will hold the result of the reduction operation
+    /// \note This is a collective operation and must be called (possibly by utilizing anther
+    /// overload) by all processes in the communicator.
     template<typename T, typename F>
     void allreduce(F f, const T &senddata, T &recvdata) const {
       MPI_Allreduce(&senddata, &recvdata, 1, detail::datatype_traits<T>::get_datatype(),
                     detail::get_op<T, F>(f).mpi_op, comm_);
     }
 
+    /// \brief Performs a reduction operation over all processes and scatters the result.
+    /// \tparam F type representing the element-wise reduction operation, reduction operation is
+    /// performed on data of type T
+    /// \tparam T type of input and output data of the reduction operation, must meet the
+    /// requirements as described in the \ref data_types "data types" section
+    /// \param f reduction operation
+    /// \param senddata input buffer for the reduction operation
+    /// \param recvdata will hold the results of the reduction operation
+    /// \param l memory layouts of the data to send and to receive
+    /// \note This is a collective operation and must be called (possibly by utilizing anther
+    /// overload) by all processes in the communicator.
     template<typename T, typename F>
     void allreduce(F f, const T *senddata, T *recvdata, const contiguous_layout<T> &l) const {
       MPI_Allreduce(senddata, recvdata, l.size(), detail::datatype_traits<T>::get_datatype(),
@@ -4000,6 +4021,18 @@ namespace mpl {
     }
 
     // --- non-blocking all-reduce ---
+    /// \brief Performs a reduction operation over all processes and scatters the result in a
+    /// non-blocking manner.
+    /// \tparam F type representing the reduction operation, reduction operation is performed
+    /// on data of type T
+    /// \tparam T type of input and output data of the reduction operation, must meet the
+    /// requirements as described in the \ref data_types "data types" section
+    /// \param f reduction operation
+    /// \param senddata input data for the reduction operation
+    /// \param recvdata will hold the result of the reduction operation
+    /// \return request representing the ongoing reduction operation
+    /// \note This is a collective operation and must be called (possibly by utilizing anther
+    /// overload) by all processes in the communicator.
     template<typename T, typename F>
     irequest iallreduce(F f, const T &senddata, T &recvdata) const {
       MPI_Request req;
@@ -4008,6 +4041,19 @@ namespace mpl {
       return impl::irequest(req);
     }
 
+    /// \brief Performs a reduction operation over all processes and scatters the result in a
+    /// non-blocking manner.
+    /// \tparam F type representing the element-wise reduction operation, reduction operation is
+    /// performed on data of type T
+    /// \tparam T type of input and output data of the reduction operation, must meet the
+    /// requirements as described in the \ref data_types "data types" section
+    /// \param f reduction operation
+    /// \param senddata input buffer for the reduction operation
+    /// \param recvdata will hold the results of the reduction operation
+    /// \param l memory layouts of the data to send and to receive
+    /// \return request representing the ongoing reduction operation
+    /// \note This is a collective operation and must be called (possibly by utilizing anther
+    /// overload) by all processes in the communicator.
     template<typename T, typename F>
     irequest iallreduce(F f, const T *senddata, T *recvdata,
                         const contiguous_layout<T> &l) const {
@@ -4018,12 +4064,33 @@ namespace mpl {
     }
 
     // --- blocking all-reduce, in place ---
+    /// \brief Performs a reduction operation over all processes and scatters the result,
+    /// in-place variant.
+    /// \tparam F type representing the reduction operation, reduction operation is performed
+    /// on data of type T
+    /// \tparam T type of input and output data of the reduction operation, must meet the
+    /// requirements as described in the \ref data_types "data types" section
+    /// \param f reduction operation
+    /// \param sendrecvdata input data for the reduction operation
+    /// \note This is a collective operation and must be called (possibly by utilizing anther
+    /// overload) by all processes in the communicator.
     template<typename T, typename F>
     void allreduce(F f, T &sendrecvdata) const {
       MPI_Allreduce(MPI_IN_PLACE, &sendrecvdata, 1, detail::datatype_traits<T>::get_datatype(),
                     detail::get_op<T, F>(f).mpi_op, comm_);
     }
 
+    /// \brief Performs a reduction operation over all processes and scatters the result,
+    /// in-place variant.
+    /// \tparam F type representing the element-wise reduction operation, reduction operation is
+    /// performed on data of type T
+    /// \tparam T type of input and output data of the reduction operation, must meet the
+    /// requirements as described in the \ref data_types "data types" section
+    /// \param f reduction operation
+    /// \param sendrecvdata input buffer for the reduction operation
+    /// \param l memory layouts of the data to send and to receive
+    /// \note This is a collective operation and must be called (possibly by utilizing anther
+    /// overload) by all processes in the communicator.
     template<typename T, typename F>
     void allreduce(F f, T *sendrecvdata, const contiguous_layout<T> &l) const {
       MPI_Allreduce(MPI_IN_PLACE, sendrecvdata, l.size(),
@@ -4032,6 +4099,17 @@ namespace mpl {
     }
 
     // --- non-blocking all-reduce, in place ---
+    /// \brief Performs a reduction operation over all processes and scatters the result in a
+    /// non-blocking manner, in-place variant.
+    /// \tparam F type representing the reduction operation, reduction operation is performed
+    /// on data of type T
+    /// \tparam T type of input and output data of the reduction operation, must meet the
+    /// requirements as described in the \ref data_types "data types" section
+    /// \param f reduction operation
+    /// \param sendrecvdata input data for the reduction operation
+    /// \return request representing the ongoing reduction operation
+    /// \note This is a collective operation and must be called (possibly by utilizing anther
+    /// overload) by all processes in the communicator.
     template<typename T, typename F>
     irequest iallreduce(F f, T &sendrecvdata) const {
       MPI_Request req;
@@ -4040,6 +4118,18 @@ namespace mpl {
       return impl::irequest(req);
     }
 
+    /// \brief Performs a reduction operation over all processes and scatters the result in
+    /// non-blocking manner, in-place variant.
+    /// \tparam F type representing the element-wise reduction operation, reduction operation is
+    /// performed on data of type T
+    /// \tparam T type of input and output data of the reduction operation, must meet the
+    /// requirements as described in the \ref data_types "data types" section
+    /// \param f reduction operation
+    /// \param sendrecvdata input buffer for the reduction operation
+    /// \param l memory layouts of the data to send and to receive
+    /// \return request representing the ongoing reduction operation
+    /// \note This is a collective operation and must be called (possibly by utilizing anther
+    /// overload) by all processes in the communicator.
     template<typename T, typename F>
     irequest iallreduce(F f, T *sendrecvdata, const contiguous_layout<T> &l) const {
       MPI_Request req;
---
+++ test/test_communicator_allreduce.cc
@@ -0,0 +1,233 @@
+#define BOOST_TEST_MODULE communicator_allreduce
+
+#include <boost/test/included/unit_test.hpp>
+#include <mpl/mpl.hpp>
+#include "test_helper.hpp"
+
+template<typename T>
+class add {
+public:
+  T operator()(const T &a, const T &b) const { return a + b; }
+};
+
+
+template<typename F, typename T>
+bool allreduce_test(F f, const T &val) {
+  const mpl::communicator &comm_world{mpl::environment::comm_world()};
+  T x{val};
+  for (int i{0}; i < comm_world.rank(); ++i)
+    ++x;
+  T y{};
+  comm_world.allreduce(f, x, y);
+  T expected{val};
+  x = val;
+  for (int i{1}; i < comm_world.size(); ++i) {
+    ++x;
+    expected = f(expected, x);
+  }
+  return y == expected;
+}
+
+
+template<typename F, typename T>
+bool allreduce_test_with_layout(F f, const T &val) {
+  const mpl::communicator &comm_world{mpl::environment::comm_world()};
+  T x{val};
+  for (int i{0}; i < comm_world.rank(); ++i)
+    ++x;
+  const int n{5};
+  mpl::contiguous_layout<T> l(n);
+  std::vector<T> v_x(n, x);
+  std::vector<T> v_y(n);
+  comm_world.allreduce(f, v_x.data(), v_y.data(), l);
+  T expected{val};
+  x = val;
+  for (int i{1}; i < comm_world.size(); ++i) {
+    ++x;
+    expected = f(expected, x);
+  }
+  std::vector<T> v_expected(n, expected);
+  return v_y == v_expected;
+}
+
+
+template<typename F, typename T>
+bool iallreduce_test(F f, const T &val) {
+  const mpl::communicator &comm_world{mpl::environment::comm_world()};
+  T x{val};
+  for (int i{0}; i < comm_world.rank(); ++i)
+    ++x;
+  T y{};
+  auto r{comm_world.iallreduce(f, x, y)};
+  r.wait();
+  T expected{val};
+  x = val;
+  for (int i{1}; i < comm_world.size(); ++i) {
+    ++x;
+    expected = f(expected, x);
+  }
+  return y == expected;
+}
+
+
+template<typename F, typename T>
+bool iallreduce_test_with_layout(F f, const T &val) {
+  const mpl::communicator &comm_world{mpl::environment::comm_world()};
+  T x{val};
+  for (int i{0}; i < comm_world.rank(); ++i)
+    ++x;
+  const int n{5};
+  mpl::contiguous_layout<T> l(n);
+  std::vector<T> v_x(n, x);
+  std::vector<T> v_y(n);
+  auto r{comm_world.iallreduce(f, v_x.data(), v_y.data(), l)};
+  r.wait();
+  T expected{val};
+  x = val;
+  for (int i{1}; i < comm_world.size(); ++i) {
+    ++x;
+    expected = f(expected, x);
+  }
+  std::vector<T> v_expected(n, expected);
+  return v_y == v_expected;
+}
+
+
+template<typename F, typename T>
+bool allreduce_test_inplace(F f, const T &val) {
+  const mpl::communicator &comm_world{mpl::environment::comm_world()};
+  T x{val};
+  for (int i{0}; i < comm_world.rank(); ++i)
+    ++x;
+  T x2{val};
+  comm_world.allreduce(f, x);
+  T expected{x2};
+  for (int i{1}; i < comm_world.size(); ++i) {
+    ++x2;
+    expected = f(expected, x2);
+  }
+  return x == expected;
+}
+
+
+template<typename F, typename T>
+bool allreduce_test_with_layout_inplace(F f, const T &val) {
+  const mpl::communicator &comm_world{mpl::environment::comm_world()};
+  T x{val};
+  for (int i{0}; i < comm_world.rank(); ++i)
+    ++x;
+  const int n{5};
+  mpl::contiguous_layout<T> l(n);
+  std::vector<T> v_x(n, x);
+  comm_world.allreduce(f, v_x.data(), l);
+  T expected{val};
+  x = val;
+  for (int i{1}; i < comm_world.size(); ++i) {
+    ++x;
+    expected = f(expected, x);
+  }
+  std::vector<T> v_expected(n, expected);
+  return v_x == v_expected;
+}
+
+
+template<typename F, typename T>
+bool iallreduce_test_inplace(F f, const T &val) {
+  const mpl::communicator &comm_world{mpl::environment::comm_world()};
+  T x{val};
+  for (int i{0}; i < comm_world.rank(); ++i)
+    ++x;
+  T x2{val};
+  auto r{comm_world.iallreduce(f, x)};
+  r.wait();
+  T expected{x2};
+  for (int i{1}; i < comm_world.size(); ++i) {
+    ++x2;
+    expected = f(expected, x2);
+  }
+  return x == expected;
+}
+
+
+template<typename F, typename T>
+bool iallreduce_test_with_layout_inplace(F f, const T &val) {
+  const mpl::communicator &comm_world{mpl::environment::comm_world()};
+  T x{val};
+  for (int i{0}; i < comm_world.rank(); ++i)
+    ++x;
+  const int n{5};
+  mpl::contiguous_layout<T> l(n);
+  std::vector<T> v_x(n, x);
+  auto r{comm_world.iallreduce(f, v_x.data(), l)};
+  r.wait();
+  T expected{val};
+  x = val;
+  for (int i{1}; i < comm_world.size(); ++i) {
+    ++x;
+    expected = f(expected, x);
+  }
+  std::vector<T> v_expected(n, expected);
+  return v_x == v_expected;
+}
+
+
+BOOST_AUTO_TEST_CASE(allreduce) {
+  BOOST_TEST(allreduce_test(add<double>(), 1.0));
+  BOOST_TEST(allreduce_test(add<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(allreduce_test(mpl::plus<double>(), 1.0));
+  BOOST_TEST(allreduce_test(mpl::plus<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(allreduce_test([](auto a, auto b) { return a + b; }, 1.0));
+  BOOST_TEST(allreduce_test([](auto a, auto b) { return a + b; }, tuple{1, 2.0}));
+
+  BOOST_TEST(allreduce_test_with_layout(add<double>(), 1.0));
+  BOOST_TEST(allreduce_test_with_layout(add<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(allreduce_test_with_layout(mpl::plus<double>(), 1.0));
+  BOOST_TEST(allreduce_test_with_layout(mpl::plus<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(allreduce_test_with_layout([](auto a, auto b) { return a + b; }, 1.0));
+  BOOST_TEST(allreduce_test_with_layout([](auto a, auto b) { return a + b; }, tuple{1, 2.0}));
+
+  BOOST_TEST(iallreduce_test(add<double>(), 1.0));
+  BOOST_TEST(iallreduce_test(add<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(iallreduce_test(mpl::plus<double>(), 1.0));
+  BOOST_TEST(iallreduce_test(mpl::plus<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(iallreduce_test([](auto a, auto b) { return a + b; }, 1.0));
+  BOOST_TEST(iallreduce_test([](auto a, auto b) { return a + b; }, tuple{1, 2.0}));
+
+  BOOST_TEST(iallreduce_test_with_layout(add<double>(), 1.0));
+  BOOST_TEST(iallreduce_test_with_layout(add<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(iallreduce_test_with_layout(mpl::plus<double>(), 1.0));
+  BOOST_TEST(iallreduce_test_with_layout(mpl::plus<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(iallreduce_test_with_layout([](auto a, auto b) { return a + b; }, 1.0));
+  BOOST_TEST(iallreduce_test_with_layout([](auto a, auto b) { return a + b; }, tuple{1, 2.0}));
+
+
+  BOOST_TEST(allreduce_test_inplace(add<double>(), 1.0));
+  BOOST_TEST(allreduce_test_inplace(add<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(allreduce_test_inplace(mpl::plus<double>(), 1.0));
+  BOOST_TEST(allreduce_test_inplace(mpl::plus<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(allreduce_test_inplace([](auto a, auto b) { return a + b; }, 1.0));
+  BOOST_TEST(allreduce_test_inplace([](auto a, auto b) { return a + b; }, tuple{1, 2.0}));
+
+  BOOST_TEST(allreduce_test_with_layout_inplace(add<double>(), 1.0));
+  BOOST_TEST(allreduce_test_with_layout_inplace(add<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(allreduce_test_with_layout_inplace(mpl::plus<double>(), 1.0));
+  BOOST_TEST(allreduce_test_with_layout_inplace(mpl::plus<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(allreduce_test_with_layout_inplace([](auto a, auto b) { return a + b; }, 1.0));
+  BOOST_TEST(
+      allreduce_test_with_layout_inplace([](auto a, auto b) { return a + b; }, tuple{1, 2.0}));
+
+  BOOST_TEST(iallreduce_test_inplace(add<double>(), 1.0));
+  BOOST_TEST(iallreduce_test_inplace(add<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(iallreduce_test_inplace(mpl::plus<double>(), 1.0));
+  BOOST_TEST(iallreduce_test_inplace(mpl::plus<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(iallreduce_test_inplace([](auto a, auto b) { return a + b; }, 1.0));
+  BOOST_TEST(iallreduce_test_inplace([](auto a, auto b) { return a + b; }, tuple{1, 2.0}));
+
+  BOOST_TEST(iallreduce_test_with_layout_inplace(add<double>(), 1.0));
+  BOOST_TEST(iallreduce_test_with_layout_inplace(add<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(iallreduce_test_with_layout_inplace(mpl::plus<double>(), 1.0));
+  BOOST_TEST(iallreduce_test_with_layout_inplace(mpl::plus<tuple>(), tuple{1, 2.0}));
+  BOOST_TEST(iallreduce_test_with_layout_inplace([](auto a, auto b) { return a + b; }, 1.0));
+  BOOST_TEST(
+      iallreduce_test_with_layout_inplace([](auto a, auto b) { return a + b; }, tuple{1, 2.0}));
+}
